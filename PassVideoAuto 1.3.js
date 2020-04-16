acf4d06acce91daf0fea5c09c781f4ab0b149acc
niu-grandpa <62378518+niu-grandpa@users.noreply.github.com>
2020-04-16 17:59:03 +0800

    Update PassVideoAuto 1.3.js

--- PassVideoAuto 1.3.js
+++ PassVideoAuto 1.3.js
@@ -7,7 +7,7 @@ var Tip = $('<div></div>') //绘制提示框窗口节点
 Tip.text("😄已启用视频自动化 @作者：M ₩ A");
 $(Tip).css({ //添加窗口样式
     "width": "100%",
-    "heigth": "100px",
+    "heigth": "80px",
     "position": "fixed",
     "top": "50%",
     "left": "50%",
@@ -28,34 +28,27 @@ var video = $(".preview"); //获取所有视频
 var close = $(".close-window"); //获取视频关闭按钮,点击关闭触发资源自动点击
 var resKey; //获取视频对应id值
 var resType; //获取视频类型
-var len = video.length; //获取所有视频数量
 var hasClass = res.hasClass("preview-file", "download-res", "preview"); //资源一概去除类名
 var nums = 0; //记录请求次数
 var to = 9999; //用于发送给服务器的视频已观看时间虚假数据
-if (hasClass) res.removeClass("preview-file").removeClass("download-res"); //移除非视频资源类名，以防点击时跳出页面 
-
-setTimeout(function() { //提示窗口出现
-    $(Tip).css("opacity", 1);
-    alert('请随意点击一个视频并关闭它，即可激活自动化操作！！！')
-}, 1000)
 
 //自动化函数
 function autoClick() {
     setTimeout(function() {
         video.click();
         alert("已完成！请等待自动刷新页面");
-        console.log("12秒后自动刷新页面")
-    }, 3000)
+        console.log("5秒后自动刷新页面")
+    }, 2500)
     setTimeout(function() {
         window.location.reload(); //刷新当前页面.
-    }, 15000)
+    }, 6000)
 }
 
 video.click(function(e) { //视频点击事件
-    nums++;
     resKey = e.target.dataset["value"];
     resType = e.target.dataset["mime"];
     if (resType == "video") {
+        nums++;
         $.ajax({
             type: 'post',
             dataType: 'json',
@@ -81,3 +74,10 @@ close.click(function() { //点击关闭按钮 调用自动化函数
     res.removeClass("preview"); //移除视频资源类名，防止自动点击视时打开视频造成报错
     autoClick();
 });
+
+setTimeout(function() { //提示窗口出现
+    $(Tip).css("opacity", 1);
+    alert('请随意点击一个视频并关闭它，即可激活自动化操作！！！')
+}, 1000);
+
+if (hasClass) res.removeClass("preview-file").removeClass("download-res"); //移除非视频资源类名，以防点击时跳出页面 
